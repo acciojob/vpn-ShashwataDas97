70e6be34db823a7f204fd2ca1e7cebe17d49f4db
Shashwata Das <shashwatadas802@gmail.com>
2024-01-04 18:03:58 +0530

    Commit 5

--- src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
+++ src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
@@ -28,7 +28,7 @@ public class ConnectionServiceImpl implements ConnectionService {
         else if (countryName.equalsIgnoreCase(user.getOriginalCountry().getCountryName().toString())) {
             return user;
         } else {
-            if (user.getServiceProviderList() == null) {
+            if (user.getServiceProviderList().size() == 0) {
                 throw new Exception("Unable to connect");
             }
             List < ServiceProvider > providers = user.getServiceProviderList();
